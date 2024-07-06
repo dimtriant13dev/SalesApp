63cfe7dbe918a5c8a8fe22ad1b081f749358c2d0
Dimitrios Triantafyllidis <dimtriant13dev@gmail.com>
2024-07-06 15:12:16 +0300

    Angular Initialize

--- SalesAPI/Program.cs
+++ SalesAPI/Program.cs
@@ -9,11 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options=>{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options=>options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
